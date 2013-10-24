5a02aaa8e36745dd0f70f84e057f6bbe19b6c935
Brian Pinkney <bushxnyc@gmail.com>
2013-10-24 16:49:11 -0400

    Created Account Object

--- sqrl/mkm/__init__.py
+++ sqrl/mkm/__init__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
-import ed25519
 import os
+import scrypt
+import ed25519
+from datetime import datetime
 
 
 class MKM:
@@ -21,7 +23,7 @@ class MKM:
         if not os.path.exists(self.path):
             os.mkdir(self.path)
 
-    def _list_accounts(self):
+    def list_accounts(self):
         # Collect a list of .skey files
         # their name is the is the name of the account
         pass
@@ -40,3 +42,45 @@ class MKM:
         seed = open(self.storageFile, "rb").read()
         key = ed25519.SigningKey(seed)
         return key.to_ascii(encoding="base64")
+
+
+class Account:
+    def __init__(self, id="", attr={}):
+        if id == "":
+            self._id = self._new_id()
+            self._init_attr()
+        else:
+            self._id = id
+            self._load(attr)
+
+    def _init_attr(self):
+        self._name = ""
+        self._created = datetime.now()
+        self._encrypted_key = ""
+        ielf._modified = datetime.now()
+
+    def _load(self, attr):
+        self._name = attr['name']
+        self._created = attr['created']
+        self._encrypted_key = attr['key']
+        self._modified = attr['modified']
+
+    def set_name(self, name):
+        if name:
+            self._name = name
+
+    def get_key(self, password):
+        decrypted_key = scrypt.decrypt(self._key, password, maxtime=1)
+        return decrypted_key
+
+    def create_key(self, password, password_confirm):
+        if password == password_confirm:
+            key = self._new_key()
+            self._key = scrypt.encrypt(key, password, maxtime=1)
+
+    def _new_key(self):
+        sk, vk = ed25519.create_keypair()
+        return sk
+
+    def _new_id(self):
+        return "Test!!!"
