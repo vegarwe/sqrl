adcc082b3db7e4dd05c1803c50e51e8896cda955
Brian Pinkney <bushxnyc@gmail.com>
2013-10-31 12:17:24 -0400

    Added label to GTK.Dialog

--- sqrl/sqrlgui.py
+++ sqrl/sqrlgui.py
@@ -2,15 +2,16 @@
 from gi.repository import Gtk
 
 
-def gui_get_pass():
+def gui_get_pass(value):
     dialog = Gtk.Dialog()
     dialog.set_modal(True)
+    label = Gtk.Label(value)
     userEntry = Gtk.Entry()
     userEntry.set_visibility(False)
     dialogBox = dialog.get_content_area()
     dialogBox.pack_end(userEntry, False, False, 0)
+    dialogBox.pack_end(label, False, False, 0)
     dialog.add_button("Ok", Gtk.ResponseType.OK)
     dialog.show_all()
     dialog.run()
     return userEntry.get_text()
-
