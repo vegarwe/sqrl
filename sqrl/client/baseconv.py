53a65926c523365193152bd81af50d5c1f1cd6b6
Vegar Westerlund <vegarwe@gmail.com>
2015-08-31 23:58:35 +0200

    Add convenience functions for working with the SQRL name/value key pairs

--- sqrl/client/baseconv.py
+++ sqrl/client/baseconv.py
@@ -5,9 +5,28 @@ BaseConverter
 Uitility to encode and decode base64url to spec
 """
 
-def decode(value):
-    value += '=' * (4 - len(value) % 4)
-    return base64.urlsafe_b64decode(value)
+def decode(data):
+    data += '=' * (4 - len(data) % 4)
+    return base64.urlsafe_b64decode(data)
 
-def encode(value):
-    return base64.urlsafe_b64encode(value).rstrip("=")
+def encode(data):
+    return base64.urlsafe_b64encode(data).rstrip("=")
+
+def decodeNameValue(data):
+    ret = {}
+    for line in decode(data).split("\r\n"):
+        if line == '': continue
+        key, value = line.split('=', 1)
+        ret[key] = value
+    return ret
+
+def encodeNameValue(data):
+    if not data.has_key("ver"):
+        raise Exception("SQRL protocol requires all protocl exchanges to cary version information")
+
+    ret = "ver=%s\r\n" % data['ver']
+    for key, value in data.iteritems():
+        if key == 'ver': continue
+        ret += "%s=%s\r\n" % (key, value)
+
+    return encode(ret)
--- tests/baseconv_test.py
+++ tests/baseconv_test.py
@@ -18,3 +18,20 @@ class TestNoPaddingBase64(unittest.TestCase):
         self.assertEqual(baseconv.decode("YWJjZA"), 'abcd')
         self.assertEqual(baseconv.decode("YWJjZGU"), 'abcde')
 
+    def test_decodeNameValue_server_response(self):
+        server = baseconv.decodeNameValue('dmVyPTENCm51dD1BaHZoZHk4U3hucy1QaXdhTUtBbG53DQp0aWY9NDI0DQpxcnk9L3Nxcmw_bnV0PUFodmhkeThTeG5zLVBpd2FNS0FsbncNCnNmbj1HUkMNCg')
+
+        expected = {
+            'ver': '1',
+            'nut': 'Ahvhdy8Sxns-PiwaMKAlnw',
+            'qry': '/sqrl?nut=Ahvhdy8Sxns-PiwaMKAlnw',
+            'sfn': 'GRC',
+            'tif': '424'}
+        self.assertEqual(expected, server)
+
+    def test_encodeNameValue_ver_always_first(self):
+        server = baseconv.encodeNameValue({'ver': '1', 'nut': 'Ahvhdy8Sxns-PiwaMKAlnw'})
+        self.assertEqual('dmVyPTENCm51dD1BaHZoZHk4U3hucy1QaXdhTUtBbG53DQo', server)
+
+        server = baseconv.encodeNameValue({'nut': 'Ahvhdy8Sxns-PiwaMKAlnw', 'ver': '1'})
+        self.assertEqual('dmVyPTENCm51dD1BaHZoZHk4U3hucy1QaXdhTUtBbG53DQo', server)
