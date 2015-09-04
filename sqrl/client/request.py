7b0e59f60bb6da8b6ca18b5b0b66dfc04e9beaf6
Vegar Westerlund <vegarwe@gmail.com>
2015-09-04 08:48:13 +0200

    Make request module _vastly_ simpler. Will see later if it's to simple

--- sqrl/client/request.py
+++ sqrl/client/request.py
@@ -1,47 +1,20 @@
-#!/usr/bin/env python
-
 import httplib
 import baseconv
-#from sqrl.test import test
 
-__sqrlver__ = "1"
+def post_form(url, body):
+    headers = { "Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
 
+    if url.isSecure():
+        http = httplib.HTTPSConnection(url.netloc, timeout=9)
+    else:
+        http = httplib.HTTPConnection(url.netloc, timeout=9)
 
-class SQRLRequest():
-    """
-    SQRLRequest
-    - Formats SQRL request
-    - Submits the SQRL reuqest
-    """
+    # TODO: Use try-catch
+    http.request("POST", url.path + "?" + url.query, body, headers)
+    response = http.getresponse()
 
-    def __init__(self):
-        self.headers = {
-                "Content-type": "application/x-www-form-urlencoded",
-                "Accept": "text/plain"}
-
-    def _path(self, url):
-        return url.path + "?" + url.query
-
-    def send(self, url, body):
-        if url.isSecure():
-            http = httplib.HTTPSConnection(url.netloc, timeout=9)
-        else:
-            http = httplib.HTTPConnection(url.netloc, timeout=9)
-
-        # TODO: Use try-catch
-        #try:
-        #    http.request("POST", self._path(url), body, self.headers)
-        #    response = http.getresponse()
-        #except Exception as e:
-        #    code, msg = e
-        #    return False, msg
-
-        #print "POST", self._path(url), body, self.headers
-        http.request("POST", self._path(url), body, self.headers)
-        response = http.getresponse()
-
-        if response.status == 200:
-            return True, response.read()
-        else:
-            return False, "%s (Error: %s)" % (response.reason, response.status)
+    if response.status == 200:
+        return True, response.read()
+    else:
+        return False, "%s (Error: %s)" % (response.reason, response.status)
 
