74ed8ec4382e037802d8692fb75485b9ba7116d5
Vegar Westerlund <vegarwe@gmail.com>
2016-01-03 11:36:17 +0100

    Check (and react to) result from query and indent commands

--- sqrl/client/client.py
+++ sqrl/client/client.py
@@ -7,13 +7,14 @@ from parser import URLParser
 
 
 class Client:
-    def __init__(self, masterkey):
+    def __init__(self, masterkey, cps=False):
         self.enc = Crypt(masterkey)
+        self.cps = cps
 
-    def login(self, urlString):
-        url = URLParser(urlString)
+    def login(self, qry):
+        url = URLParser(qry)
 
-        success, data = self.query(url)
+        success, data = self.query(url, baseconv.encode(url.orig_url))
         if not success:
             return False, "Auth failed. " + data
 
@@ -21,25 +22,41 @@ class Client:
         if not success:
             return False, "Auth failed. " + data
 
-        resp = baseconv.decodeNameValue(data)
-        if not resp.has_key('url'):
-            return False, "Auth failed. No success url returned"
+        return True, data
 
-        return True, resp['url']
-
-    def query(self, url):
+    def query(self, url, server, automatic_retry=True):
         client  = "ver=1\r\n"
         client += "cmd=query\r\n"
         client += "idk=%s\r\n" % self.enc.getPublicKey(url.getDomain())
-        #client += "opt=cps\r\n"
+        if self.cps:
+            client += "opt=cps\r\n"
         client = baseconv.encode(client)
-
-        server = url.orig_url
-        server = baseconv.encode(server)
 
         ids = self.enc.sign(client + server)
 
-        return self._post_form(url, "client=%s&server=%s&ids=%s" % (client, server, ids))
+        success, data = self._post_form(url, "client=%s&server=%s&ids=%s" % (client, server, ids))
+        logging.debug('  client %r', baseconv.decode(client))
+        logging.debug('  server %r', baseconv.decode(server))
+        logging.debug('  ids    %r', ids)
+
+        if success:
+            resp = baseconv.decodeNameValue(data)
+            logging.debug("resp %r", resp)
+            tif = int(resp['tif'], 16)
+            if tif & 0x01 == 0x01:
+                logging.info("ID match")
+            if tif & 0x04 == 0x04:
+                logging.info("IP matched")
+            if tif > 0x04: # TODO: Check tif failure values
+                logging.warn("Problems with query, tif 0x%02x", tif)
+
+            if tif & 0x20 == 0x20:
+                if automatic_retry:
+                    logging.debug("0x20 Transient error. Trying again")
+                    url = URLParser(url.scheme + "://" + url.netloc + resp['qry'])
+                    return self.query(url, data, False)
+        return success, data
+
 
     def ident(self, url, server):
         resp = baseconv.decodeNameValue(server)
@@ -47,15 +64,37 @@ class Client:
         client  = "ver=1\r\n"
         client += "cmd=ident\r\n"
         client += "idk=%s\r\n" % self.enc.getPublicKey(url.getDomain())
-        #client += "opt=cps\r\n"
-        client += "suk=dMRXbs49XNmVUhsKzta7ESD-cP2QlnxkSaORsswOAj4\r\n"
-        client += "vuk=q13E_hd5CR0WE0A9ZD8571te0Ul47YfsDCWpETuCGcI\r\n"
+        if self.cps:
+            client += "opt=cps\r\n"
+        client += "suk=dMRXbs49XNmVUhsKzta7ESD-cP2QlnxkSaORsswOAj4\r\n" # TODO: ehhh...
+        client += "vuk=q13E_hd5CR0WE0A9ZD8571te0Ul47YfsDCWpETuCGcI\r\n" # TODO: ehhh...
         client = baseconv.encode(client)
 
         ids = self.enc.sign(client + server)
 
         url = URLParser(url.scheme + "://" + url.netloc + resp['qry'])
-        return self._post_form(url, "client=%s&server=%s&ids=%s" % (client, server, ids))
+        success, data = self._post_form(url, "client=%s&server=%s&ids=%s" % (client, server, ids))
+        logging.debug('  client %r', baseconv.decode(client))
+        logging.debug('  server %r', baseconv.decode(server))
+        logging.debug('  ids    %r', ids)
+
+        if success:
+            resp = baseconv.decodeNameValue(server)
+            logging.debug("resp %r", resp)
+
+            tif = int(resp['tif'], 16)
+            if tif & 0x08 == 0x08:
+                return False, "0x08 SQRL disabled"
+            if tif & 0x10 == 0x10:
+                return False, "0x10 Function(s) not supported"
+            if tif > 0x04: # TODO: Check tif failure values
+                return False, "Tif 0x%02x" % tif
+
+            if 'url' in resp:
+                return True, resp['url']
+            else:
+                return True, None
+        return False, data
 
     def _post_form(self, url, body):
         headers = { "Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
