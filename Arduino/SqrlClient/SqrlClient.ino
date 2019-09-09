050bcba1795100934d7a5c3817cf1a4878c7022f
Vegar Westerlund <vegarwe@gmail.com>
2019-09-09 08:11:13 +0200

    Start updating Arduino variant

--- Arduino/SqrlClient/SqrlClient.ino
+++ Arduino/SqrlClient/SqrlClient.ino
@@ -1,9 +1,11 @@
-#include <ArduinoJson.h>
 #include <RNG.h>
 //#include <TransistorNoiseSource.h>
 //#include <RingOscillatorNoiseSource.h>
 
+#include "HardwareSerial.h"
+
 #include "sqrl_client.h"
+#include "sqrl_comm.h"
 
 
 HardwareSerial* debugger = NULL;
@@ -16,102 +18,131 @@ HardwareSerial* inStream;
 char buffer[1024];
 size_t buffer_idx = 0;
 
+// Hard coded keys for now
 uint8_t iuk[] = {0xa2,0x43,0xbf,0xb0,0xed,0x04,0x56,0x5d,0x04,0x61,0xa7,0x73,0x1a,0x8f,0xad,0x18,0xde,0x4b,0xdd,0xf8,0xe3,0x83,0x38,0x53,0x92,0x6c,0xcf,0xaf,0x2e,0x2e,0x04,0xa6};
 uint8_t imk[] = {0x21,0xd7,0x08,0x94,0x57,0x5e,0x6b,0x6e,0xfe,0x99,0x1f,0xb8,0x6a,0x98,0x68,0xa4,0x9f,0x3a,0x72,0x04,0x0e,0x88,0x25,0x2d,0x82,0xbe,0x5a,0x3a,0xc6,0xc3,0xaa,0x23};
 uint8_t ilk[32];
+
+static volatile sqrl_cmd_t* mp_cmd;
+
+
+static void on_sqrl_comm_evt(sqrl_comm_evt_t * p_evt)
+{
+    if (p_evt->evt_type == SQRL_COMM_EVT_COMMAND)
+    {
+        mp_cmd = p_evt->evt.p_cmd;
+    }
+}
+
+
+static void handle_command(sqrl_cmd_t* p_cmd)
+{
+    if (p_cmd == NULL)
+    {
+        return;
+    }
+
+    if (p_cmd->type == SQRL_CMD_QUERY)
+    {
+        if (debugger) debugger->println("Got cmd: 'QUERY'");
+
+        ClientResponse resp = sqrl_query(imk, p_cmd->sks, p_cmd->server);
+
+        inStream->printf("\x02resp\x1equery\x1e%s\x1e%s\x1e%s\x03\n",
+                resp.client.c_str(), p_cmd->server, resp.ids.c_str());
+
+        if (debugger) debugger->println(p_cmd->sks);
+        if (debugger) debugger->print( "resp.client ");
+        if (debugger) debugger->println(resp.client.c_str());
+
+        //inStream->print("\x02resp\x1equery\x1e");
+        //inStream->print(resp.client);
+        //inStream->print("\x1e");
+        //inStream->print(p_cmd->server);
+        //inStream->print("\x1e");
+        //inStream->print(resp.ids);
+        //inStream->print("\x03\n");
+    }
+    else if (p_cmd->type == SQRL_CMD_IDENT)
+    {
+        if (debugger) debugger->println("Got cmd: 'IDENT'");
+
+        uint8_t rlk[32];
+        while (! RNG.available(sizeof(rlk))) {
+            if (debugger) debugger->println("Gather randomness");
+            delay(500);
+        }
+        RNG.rand(rlk, sizeof(rlk));
+
+        ClientResponse resp = sqrl_ident(ilk, imk, rlk, p_cmd->sks, p_cmd->server, p_cmd->sin, p_cmd->create_suk);
+
+        inStream->printf("\x02resp\x1eident\x1e%s\x1e%s\x1e%s\x03\n",
+                resp.client.c_str(), p_cmd->server, resp.ids.c_str());
+
+        if (debugger) debugger->println(p_cmd->sks);
+        if (debugger) debugger->print( "resp.client ");
+        if (debugger) debugger->println(resp.client.c_str());
+    }
+    else
+    {
+        if (debugger) debugger->println("err: Invalid command");
+
+        inStream->print("\x02log\x1e err: Invalid command\x03\n");
+    }
+}
 
 
 void setup() {
     inStream = &Serial;
     debugger = &Serial1;
 
-    inStream->begin(115200);
-    while (!inStream);
-
 	if (debugger) {
 		// Setup serial port for debugging
 		debugger->begin(115200);
 		while (!debugger);
 		debugger->println("");
-		debugger->println("Started...");
+		debugger->println("Staring");
 	}
+
+    inStream->begin(115200);
+    while (!inStream); // TODO: Dereference to get bool operator overload?
 
     RNG.begin("SqrlClient");
     //RNG.addNoiseSource(noise); // TODO: Need noise source!!!
 
     sqrl_get_ilk_from_iuk(ilk, iuk);
+
+	if (debugger) debugger->println("Started");
+    //if (debugger) debugger->print( "SERIAL_TX_BUFFER_SIZE ");
+    //if (debugger) debugger->println(SERIAL_TX_BUFFER_SIZE);
+
+    sqrl_comm_init(on_sqrl_comm_evt);
 }
 
 
 void loop() {
     RNG.loop();
 
-	if (inStream->available())
+    int numBytes = 0;
+	if (numBytes = inStream->available())
 	{
-		char newByte = inStream->read();
-        buffer[buffer_idx] = newByte;
-        buffer_idx++;
-
-        //if (debugger) debugger->print("Got byte: '");
-        //if (debugger) debugger->print(newByte);
-        //if (debugger) debugger->println("'");
-
-		if (newByte == '\n')
+        while (numBytes--)
         {
-            if (debugger) debugger->print("Got line: ");
-            for (int i = 0; i < buffer_idx; i++) {
-                if (debugger) debugger->print(buffer[i]);
-            }
+            char newByte = inStream->read();
+            sqrl_comm_handle_input(&newByte, 1);
 
-            DynamicJsonDocument doc(1024);
-            DeserializationError error = deserializeJson(doc, buffer, buffer_idx);
-            buffer_idx = 0;
-            if (error)
-            {
-                if (debugger) debugger->print(F("deserializeJson() failed: "));
-                if (debugger) debugger->println(error.c_str());
-                return;
-            }
-
-            ClientResponse resp;
-            if (doc["cmd"] == "query") {
-                if (debugger) debugger->println("query");
-
-                String sks = doc["sks"];
-                String server = doc["server"];
-                if (debugger) debugger->println(sks);
-                resp = sqrl_query(imk, sks.c_str(), server.c_str());
-            } else if (doc["cmd"] == "ident") {
-                if (debugger) debugger->println("ident");
-
-                uint8_t rlk[32];
-                while (! RNG.available(sizeof(rlk))) {
-                    if (debugger) debugger->println("Gather randomness");
-                    delay(500);
-                }
-                RNG.rand(rlk, sizeof(rlk));
-
-                const char* sks = doc["sks"];
-                const char* server = doc["server"];
-                const char* sin_ = doc["sin"];
-                bool create_suk = doc["create_suk"];
-                resp = sqrl_ident(ilk, imk, rlk, sks, server, sin_, create_suk);
-                if (debugger) debugger->println("Done...");
-            }
-
-            doc.clear();
-            doc["client"] = resp.client.c_str();
-            doc["server"] = resp.server.c_str();
-            doc["ids"] = resp.ids.c_str();
-
-            serializeJson(doc, buffer, sizeof(buffer));
-            inStream->println(buffer);
-            buffer_idx = 0;
-            memset(buffer, 0, sizeof(buffer));
+            //if (debugger) debugger->print("Got byte: '");
+            //if (debugger) debugger->print(newByte);
+            //if (debugger) debugger->println("'");
         }
 	}
-    //else
-    //{
-    //    delay(100);
-    //}
+
+    if (mp_cmd != NULL)
+    {
+        handle_command();
+
+        mp_cmd = NULL;
+        sqrl_comm_command_handled();
+    }
 }
+
---
+++ Arduino/SqrlClient/sqrl_comm.cpp
@@ -0,0 +1,184 @@
+#include <string.h>
+
+#include "sqrl_comm.h"
+
+
+#define STX '\x02' // Start of text
+#define ETX '\x03' // End of text
+#define NAK '\x15' // Negative acknowledge
+#define RS  '\x1E' // Record seperator
+
+#define MAX_TOKENS 15
+
+
+typedef enum {
+    sqrl_comm_state_initial,
+    sqrl_comm_state_find_end,
+    sqrl_comm_state_handle_cmd,
+} sqrl_comm_state_t;
+
+
+static volatile sqrl_comm_state_t   m_state = sqrl_comm_state_initial;
+static sqrl_comm_evt_handler_t      m_evt_handler;
+
+
+static void handle_cmd(char* buffer);
+
+
+void sqrl_comm_handle_input(char* in_buffer, size_t in_len)
+{
+    static char     s_buffer[2048]; // TODO: Find proper size
+    static uint32_t s_bufidx = 0;
+
+    if (m_evt_handler == NULL)
+    {
+        return; // TODO: Report error somehow
+    }
+
+    for (size_t i = 0; i < in_len; i++) // TODO: Is this really the best way? And also, we discard any trailing garbage here. Maybe that's what we want?
+    {
+        char c = in_buffer[i];
+
+        if (m_state == sqrl_comm_state_initial)
+        {
+            if (c == STX)
+            {
+                s_bufidx = 0;
+                m_state = sqrl_comm_state_find_end;
+            }
+            // Ignore all other characters untill STX
+        }
+        else if (m_state == sqrl_comm_state_find_end)
+        {
+            s_buffer[s_bufidx++] = c;
+
+            if (s_bufidx > sizeof(s_buffer))
+            {
+                m_state = sqrl_comm_state_initial;
+                // TODO: Report error somehow
+            }
+
+            if (c == '\0')
+            {
+                m_state = sqrl_comm_state_initial;
+                // TODO: Report error somehow
+            }
+
+            if (c == ETX)
+            {
+                m_state = sqrl_comm_state_handle_cmd;
+                s_buffer[s_bufidx-1] = '\0';
+
+                handle_cmd(s_buffer);
+            }
+        }
+    }
+}
+
+static char *strseparator(char ** p_s)
+{
+    char * ret = *p_s;
+
+    if (*p_s == NULL)
+    {
+        return NULL;
+    }
+
+    while (**p_s != '\0')
+    {
+        if (**p_s == RS)
+        {
+            **p_s = '\0';
+            (*p_s)++;
+            return ret;
+        }
+        else
+        {
+            (*p_s)++;
+        }
+    }
+
+    *p_s = NULL;
+    return ret;
+}
+
+
+static void handle_cmd(char* buffer)
+{
+    static sqrl_cmd_t s_cmd;
+
+    sqrl_comm_evt_t evt;
+
+    char*       tokens[MAX_TOKENS];
+    uint32_t    token_cnt = 0;
+
+    // Split into tokens
+    while ((tokens[token_cnt] = strseparator(&buffer)) != NULL)
+    {
+        token_cnt++;
+        if (token_cnt >= MAX_TOKENS)
+        {
+            break;
+        }
+    }
+
+    // Debug output of parsed input
+    //printf("token_cnt %d\n", token_cnt);
+    //for (int i = 0; i < token_cnt; i++)
+    //{
+    //    printf("  tokens: '%s'\n", tokens[i]);
+    //}
+
+    // Handle invalid (too short) command
+    if (token_cnt < 3)
+    {
+        return; // TODO: Report error somehow
+    }
+
+    // Assign fixed params
+    s_cmd.sks = tokens[1];
+    s_cmd.server = tokens[2];
+
+    // Check command type, handle variable params
+    if (strcmp(tokens[0], "query") == 0)
+    {
+        s_cmd.type = SQRL_CMD_QUERY;
+
+        evt.evt_type = SQRL_COMM_EVT_COMMAND;
+        evt.evt.p_cmd = &s_cmd;
+        m_evt_handler(&evt);
+    }
+    else if (strcmp(tokens[0], "ident") == 0)
+    {
+        s_cmd.type = SQRL_CMD_IDENT;
+
+        if (token_cnt < 5)
+        {
+            return; // TODO: Report error somehow
+        }
+
+        s_cmd.sin = tokens[3];
+        s_cmd.create_suk = strcmp(tokens[4], "true") == 0;
+
+        evt.evt_type = SQRL_COMM_EVT_COMMAND;
+        evt.evt.p_cmd = &s_cmd;
+        m_evt_handler(&evt);
+    }
+    else
+    {
+        return; // TODO: Report error somehow
+    }
+}
+
+uint32_t sqrl_comm_init(sqrl_comm_evt_handler_t evt_handler)
+{
+    m_state = sqrl_comm_state_initial;
+    m_evt_handler = evt_handler;
+
+    return 0;
+}
+
+void sqrl_comm_command_handled(void)
+{
+    m_state = sqrl_comm_state_initial;
+}
