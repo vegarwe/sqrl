cb90544b388dec4afdd1e316550e86baf18c252e
Vegar Westerlund <vegarwe@gmail.com>
2019-10-09 23:03:37 +0200

    Fix up Keil projects after code move

--- nrf_sqrl/sqrl_comm_test.py
+++ nrf_sqrl/sqrl_comm_test.py
@@ -10,8 +10,8 @@ with serial.Serial(port=serial_port, baudrate=115200, timeout=.3) as ser:
 
     #cmd = b"\ngarbage\x02query\x1ewww.grc.com\x1ec3FybDovL3d3dy5ncmMuY29tL3Nxcmw_bnV0PW9HWEVVRW1Ua1BHMHowRWthM3BISlE\x03\nmore Garbage!\n"
     #cmd = b"\x02query\x1ewww.grc.com\x1ec3FybDovL3d3dy5ncmMuY29tL3Nxcmw_bnV0PW9HWEVVRW1Ua1BHMHowRWthM3BISlE\x03"
-    #cmd = b"\x02ident\x1ewww.grc.com\x1edmVyPTENCm51dD1fUXhuNlJwUVJGZHk5NHRiekllN29RDQp0aWY9NQ0KcXJ5PS9zcXJsP251dD1fUXhuNlJwUVJGZHk5NHRiekllN29RDQpzdWs9UEJGdWZRNmR2emgtYXB3dU1tXzR6MmFybmZNdjRDVUxVRTRWZVVFYWdWOA0KdXJsPWh0dHBzOi8vd3d3LmdyYy5jb20vc3FybC9kaWFnLmh0bT9fUXhuNlJwUVJGZHk5NHRiekllN29RDQpzaW49MA0K\x1e0\x1etrue\x03"
-    cmd = b"\x02unlock\x1e<scrypt-binary-key-goes-here>\x03"
+    cmd = b"\x02ident\x1ewww.grc.com\x1edmVyPTENCm51dD1fUXhuNlJwUVJGZHk5NHRiekllN29RDQp0aWY9NQ0KcXJ5PS9zcXJsP251dD1fUXhuNlJwUVJGZHk5NHRiekllN29RDQpzdWs9UEJGdWZRNmR2emgtYXB3dU1tXzR6MmFybmZNdjRDVUxVRTRWZVVFYWdWOA0KdXJsPWh0dHBzOi8vd3d3LmdyYy5jb20vc3FybC9kaWFnLmh0bT9fUXhuNlJwUVJGZHk5NHRiekllN29RDQpzaW49MA0K\x1e0\x1etrue\x03"
+    #cmd = b"\x02unlock\x1e<scrypt-binary-key-goes-here>\x03"
 
     #print(len(cmd), hex(len(cmd)))
     print(repr(cmd))
